79891557fbd617dd7f3ace9e235a45fb72378039
むずでょ <muzudho1@gmail.com>
2023-05-08 18:51:04 +0900

    c_while

---
+++ loop_0_to_9/c_while.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * loop_0_to_9
+ * C
+ **/
+int main()
+{
+    int i = 0;
+    while (i < 10)
+    {
+        printf("i=%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
